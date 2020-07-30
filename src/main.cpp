5146c02558f2283af8aeca9c35e01136d2136f76
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-30 11:01:59 +0300

    Add tests and basic main to open file and check if it gzipped

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,43 @@
 #include <cstdlib>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 
 int main() {
+    auto path = std::string("/home/valery/Projects/opensource/github/hype/tests/RR_CPU.pb.gz");
+    std::ifstream myfile;
+
+
+    std::vector<char> buffer;
+    try {
+        myfile.open(path, std::ios_base::binary);
+        if (myfile.is_open()) {
+            myfile.seekg(0, std::ios_base::end);
+            size_t length = myfile.tellg();
+            myfile.seekg(0, std::ios_base::beg);
+
+            if (length > 0) {
+                buffer.resize(length);
+                myfile.read(&buffer[0], length);
+
+                // check if file is gzipped
+                if ((buffer[0] == char (0x1f)) and (buffer[1] == char (0x8b))) {
+                    std::cout << "here" << std::endl;
+                }
+            }
+        }
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+        myfile.close();
+        return EXIT_FAILURE;
+    }
+
+
+    myfile.
+
+            close();
+
     return EXIT_SUCCESS;
 }
