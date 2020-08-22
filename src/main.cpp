c2bfd0fe416d06a7c8533079725d4aca840c1e23
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-22 22:30:12 +0300

    Update CMakeLists, use vcpkg integration

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 
     description.add_options()(
             "path", opt::value<std::string>(), "path to the pprof file"
-            );
+    );
 
     opt::variables_map vm;
 
